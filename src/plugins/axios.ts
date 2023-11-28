dddc46135da0554f3d0554b43cb2de36d8c76d43
Zenwinks <Lulu180299>
2023-11-28 17:03:08 +0100

    update redirection (netlify issue)

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -7,7 +7,7 @@ const api = axios.create({
 api.interceptors.response.use((response) => response, (error) => {
   if(error.response.status === 401) {
     localStorage.removeItem('user');
-    window.location.href = '/login'; // bad but can't access the router
+    window.location.href = '/'; // bad but can't access the router
   }
 });
 
